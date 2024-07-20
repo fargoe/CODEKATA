011ae2374171c8577393b53c675e965514cc51c4
fargoe <fargoewave@gmail.com>
2024-07-20 22:38:11 +0900

    REMOVE : 파일 삭제

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-
-class Solution {
-    public double solution(int[] arr) {
-        return Arrays.stream(arr).average().orElse(0.0);
-    }
-}
--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public String solution(String phone_number) {
-        int length = phone_number.length();
-        String mask = "*".repeat(length - 4); 
-        String substr = phone_number.substring(length - 4); 
-        return mask + substr; 
-    }
-}
