fcb76af2841e25fe0d509a814407e9dcca5e63ca
fargoe <fargoewave@gmail.com>
2024-08-12 11:49:31 +0900

    [level 1] Title: 특정 옵션이 포함된 자동차 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/1/157343. 특정 옵션이 포함된 자동차 리스트 구하기/특정 옵션이 포함된 자동차 리스트 구하기.sql
+++ MySQL/프로그래머스/1/157343. 특정 옵션이 포함된 자동차 리스트 구하기/특정 옵션이 포함된 자동차 리스트 구하기.sql
@@ -1,4 +1,4 @@
-select *
-from car_rental_company_car
-where options like '%네비게이션%'
-order by car_id desc;
+SELECT CAR_ID, CAR_TYPE, DAILY_FEE, OPTIONS
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE OPTIONS LIKE '%네비게이션%'
+ORDER BY CAR_ID DESC
