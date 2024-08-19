ea21df02668ff8e152f7125d6b0b1c6ba6513052
fargoe <fargoewave@gmail.com>
2024-08-20 00:33:28 +0900

    [level 2] Title: H-Index, Time: 1.31 ms, Memory: 71.4 MB -BaekjoonHub

--- Java/프로그래머스/2/42747. H－Index/H－Index.java
+++ Java/프로그래머스/2/42747. H－Index/H－Index.java
@@ -14,4 +14,11 @@ class Solution {
         }
         return hIndex;
     }
+    
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] citations = {3, 5};
+        int result = sol.solution(citations);
+        System.out.println("H-Index: " + result);  // 출력: H-Index: 3
+    }
 }
