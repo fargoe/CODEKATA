b18be98adc77b55fc691bc43f043aa1df0c4dcf5
fargoe <fargoewave@gmail.com>
2024-09-01 23:21:34 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM (
+        SELECT 
+            DATE_FORMAT(SALES_DATE, '%Y-%m-%d') 'SALES_DATE',
+            PRODUCT_ID, USER_ID, SALES_AMOUNT
+            FROM ONLINE_SALE
+            WHERE MONTH(SALES_DATE) = 3
+        UNION ALL
+        SELECT 
+            DATE_FORMAT(SALES_DATE, '%Y-%m-%d') 'SALES_DATE',
+            PRODUCT_ID, NULL 'USER_ID', SALES_AMOUNT
+            FROM OFFLINE_SALE
+            WHERE MONTH(SALES_DATE) = 3
+        ) A
+ORDER BY SALES_DATE, PRODUCT_ID, USER_ID;
