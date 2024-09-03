69a01ed05277dc2d82e83200d23f39aff9d7283c
fargoe <fargoewave@gmail.com>
2024-09-04 01:02:10 +0900

    [level 1] Title: 소수 만들기, Time: 1.88 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Solution {
+    public boolean isPrime(int num) {
+        if (num <= 1) return false;
+        if (num == 2) return true;
+        if (num % 2 == 0) return false;
+        for (int i = 3; i <= Math.sqrt(num); i += 2) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public int solution(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+}
